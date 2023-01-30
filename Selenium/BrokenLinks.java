1cb09ff38e8ce54247c32664b8b3a213771fdabe
tanmayubhe07 <88426142+tanmayubhe07@users.noreply.github.com>
2023-01-31 00:09:20 +0530

    Add files via upload
    soft assertions & broken link

---
+++ Selenium/BrokenLinks.java
@@ -0,0 +1,39 @@
+package selenium;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BrokenLinks {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+
+		WebDriver driver = new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		
+		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+		
+		String url = driver.findElement(By.linkText("Broken Link")).getAttribute("href");
+		
+		HttpURLConnection conn= (HttpURLConnection) new URL(url).openConnection();
+		
+		//URL is the class and openConnection is the method  
+		
+		
+		conn.setRequestMethod("HEAD");
+		conn.connect();  // connects with that URL 
+		int statusCode = conn.getResponseCode();
+		System.out.println(statusCode);
+			
+		
+	}
+
+}
---
+++ Selenium/SoftAssertions.java
@@ -0,0 +1,47 @@
+package selenium;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.asserts.SoftAssert;
+
+public class SoftAssertions {
+
+	public static void main(String[] args) throws MalformedURLException, IOException {
+		// TODO Auto-generated method stub
+
+		WebDriver driver = new ChromeDriver();
+
+		driver.manage().window().maximize();
+		
+		SoftAssert sa = new SoftAssert();
+		
+		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+
+		List<WebElement> links = driver.findElements(By.cssSelector(".gf-li a"));
+
+		for (WebElement link : links) {
+			
+			String url = link.getAttribute("href");
+			
+			HttpURLConnection conn = (HttpURLConnection)new URL(url).openConnection();
+			conn.setRequestMethod("HEAD");
+			conn.connect();
+			int statusCode = conn.getResponseCode();
+			System.out.println(statusCode);
+			sa.assertTrue(statusCode<400, "The link with Text "+link.getText()+" is Broken with code "+statusCode);
+
+		}
+		sa.assertAll();
+		
+
+	}
+
+}
