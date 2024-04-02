b61f3c6d343b7124729bc3fb3558c6cf2417b877
Tanmay Sunil Ubhe <88426142+tanmayubhe07@users.noreply.github.com>
2024-04-02 19:49:28 +0300

    Add files via upload

--- Selenium/eCommerceAddToCart.java
+++ Selenium/eCommerceAddToCart.java
@@ -8,10 +8,14 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class eCommerceAddToCart {
 
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
+		
+		 WebDriverManager.chromedriver().setup();
 		
 		WebDriver driver = new ChromeDriver();
 		
